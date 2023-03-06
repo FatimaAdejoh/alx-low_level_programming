1221449674c6806566116714b6ce947f395a5408
Fatima Adejoh <adejohfatima33@gmail.com>
2023-03-06 07:47:37 -0800

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,16 +9,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, n;
+	int k;
 
-	for (n = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
-		for (n = 0; accept[n] != '\0'; n++)
+		for (k = 0; accept[k]; k++)
 		{
-			if (s[i] == accept[n])
-				return (s + i);
+		if (*s == accept[k])
+		return (s);
 		}
+	s++;
 	}
-
-	return (NULL);
+	return ('\0');
 }
